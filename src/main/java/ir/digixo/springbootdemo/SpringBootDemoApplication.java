119b7e354a97fa378555d8f3fdc3c764ce68fd85
Nima Foroohari <nforoohari@gmail.com>
2025-04-22 10:25:30 +0330

    Version 2

--- src/main/java/ir/digixo/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/ir/digixo/springbootdemo/SpringBootDemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class})
+@SpringBootApplication /*(exclude = {WebMvcAutoConfiguration.class})*/
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
