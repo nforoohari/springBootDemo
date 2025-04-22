6b23b4192e80737841ca1f804536281abcb0275c
Nima Foroohari <nforoohari@gmail.com>
2025-04-22 09:48:03 +0330

    Version 0

---
+++ src/main/java/ir/digixo/springbootdemo/SpringBootDemoApplication.java
@@ -0,0 +1,17 @@
+package ir.digixo.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+
+@SpringBootApplication//(exclude = {WebMvcAutoConfiguration.class})
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+    //
+
+    //bean
+
+}
